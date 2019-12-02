230a504d4144504c580e6e5dd8a40f3eb5d10425
amdiag <amdiag@ya.ru>
2019-12-02 23:18:59 +0300

    Updated tests code

--- Class1.cs
+++ Class1.cs
@@ -3,41 +3,85 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections;
+using System.Threading;
 
 namespace WebDriver_CSharp_Example
 {
     [TestFixture]
     public class Chrome_Sample_test
     {
-        const string xpathCountrySelector = "//*[@id='country-element']//span[@class='selecter-selected']";
+        const int timeToWaitSec = 15;
+        const string xpathCountrySelector = "//*[@id='country-element']//*[@class='selecter-selected']";
+        const string xpathCountrySelect = "//*[@id='country-element']//span[@data-value='Romania']";
+
+        const string xpathLanguageSelector = "//div[@id='language']//span[@class='selecter-selected']";
+        const string xpathLanguageSelect = "//div[@id='language']//*[text()[contains(.,'English')]]";
+
+        const string xpathApplyBtn = "//a[text()[contains(.,'Apply')]]";
+        const string xpathShowAllBtn = "//a[text()='Show all jobs']";
+        const string xpathAllVacancies = "//*[contains(@class,'vacancies-blocks ')]//div[contains(@class,'vacancies-blocks-col')]";
+
         private IWebDriver browser;
+        private WebDriverWait wait;
         public string homeURL;
 
-        [Test(Description = "Check SauceLabs Homepage for Login Link")]
-        public void Login_is_on_home_page()
+       
+        [TestCaseSource(typeof(MyDataClass), "TestCases")]
+        [Test(Description = "Check quantity of vacancy")]
+        public void Test_select_country_language_check_vacancy_on_page(string country, string language, int numVacancy)
         {
             browser.Navigate().GoToUrl(homeURL);
-            browser.Manage().Window.Maximize();
-            WebDriverWait wait = new WebDriverWait(browser, System.TimeSpan.FromSeconds(15));
-            wait.Until(browser => browser.FindElement(By.XPath(xpathCountrySelector)));
-            IWebElement CountrySelector = browser.FindElement(By.XPath(xpathCountrySelector));
-            CountrySelector.Click();
-            CountrySelector.
+            wait = new WebDriverWait(browser, System.TimeSpan.FromSeconds(15));
 
-            /*WebDriverWait wait = new WebDriverWait(browser,
-System.TimeSpan.FromSeconds(15));
-            wait.Until(browser =>
-browser.FindElement(By.XPath("//a[@href='/beta/login']")));
-            IWebElement element =
-browser.FindElement(By.XPath("//a[@href='/beta/login']"));
-            Assert.AreEqual("Sign In", element.GetAttribute("text"));*/
+            CheckAndClickCountry(country);
+            CheckAndClickLang(language);
+            CheckVacancy(numVacancy);
+
+            Thread.Sleep(10000);
+        }
+
+        private void CheckAndClickCountry( string country) {
+            wait.Until(browser => browser.FindElement(By.XPath(xpathCountrySelector)));
+            browser.FindElement(By.XPath(xpathCountrySelector)).Click();            
+            IWebElement CountrySelect = browser.FindElement(By.XPath(xpathCountrySelect));
+            CountrySelect.Click();
+        }
+        private void CheckAndClickLang(string language) {
+            wait.Until(browser => browser.FindElement(By.XPath(xpathLanguageSelector)));
+            browser.FindElement(By.XPath(xpathLanguageSelector)).Click();            
+            IWebElement LanguageSelect = browser.FindElement(By.XPath(xpathLanguageSelect));
+            LanguageSelect.Click();
+
+            IWebElement ApplyBtn = browser.FindElement(By.XPath(xpathApplyBtn));
+            ApplyBtn.Click();
+            var showAllBtn = browser.FindElement(By.XPath(xpathShowAllBtn));
+
+            while (browser.FindElement(By.XPath(xpathShowAllBtn)).Displayed)
+            {
+                showAllBtn.Click();
+                Thread.Sleep(2000);
+            }
+        }
+        private void CheckVacancy( int numVacancy)
+        {
+            var vacancies = browser.FindElements(By.XPath(xpathAllVacancies));
+            Assert.True(vacancies.Count == numVacancy, $"Quantity of vacancy isn't equals. Founded - {vacancies.Count}");
         }
 
 
         [TearDown]
         public void TearDownTest()
         {
-            browser.Close();
+            try
+            {
+                browser.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Exception while stopping chrome driver {e.Message}");
+            }
+            
         }
 
 
@@ -46,7 +90,19 @@ browser.FindElement(By.XPath("//a[@href='/beta/login']"));
         {
             homeURL = "https://careers.veeam.com/";
             browser = new ChromeDriver();
-            browser.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15); 
+            browser.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
+            browser.Manage().Window.Maximize();
+        }
+    }
+}
+
+public class MyDataClass
+{
+    public static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData("Romania", "English", 29);
         }
     }
 }
